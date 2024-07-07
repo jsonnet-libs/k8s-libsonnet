5345d2d4dc5b4477827eb988fe64eb8decf78ca1
Filippo Pacifici <fpacifici@sentry.io>
2024-07-06 21:52:29 -0700

    Add python module

---
+++ sbin/bump-version.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -eux
+
+OLD_VERSION="${1}"
+NEW_VERSION="${2}"
+
+echo "Current version: $OLD_VERSION"
+echo "Bumping version: $NEW_VERSION"
+
+function replace() {
+    ! grep "$2" $3
+    perl -i -pe "s/$1/$2/g" $3
+    grep "$2" $3  # verify that replacement was successful
+}
+
+replace "version = \"[0-9.]+\""  "version = \"$NEW_VERSION\"" pyproject.toml
